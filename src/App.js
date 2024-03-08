42833b3bbea454b5a57d8fcb112a59767e563062
Mohammed-Aslam-PA <141703885+Mohammed-Aslam-PA@users.noreply.github.com>
2024-03-08 16:11:43 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen bg-[#FFFFFF] flex items-center justify-center">
+      <a
+        href="https://kghawes.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="bg-[#110E2E] rounded-[0.75rem] px-[2.5rem] py-[0.88rem] text-base font-semibold text-[#fff] cursor-pointer"
+      >
+        KG Hawes
+      </a>
     </div>
   );
 }
